1cc1fa4b36a819eba516beb0c2a2134fc8491538
Max vT <mvt@edevax.de>
2017-06-14 11:38:13 +0200

    ups

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace CommicDB
 
         public Startup(IHostingEnvironment env)
         {
-            env.EnvironmentName = "Production";
+            env.EnvironmentName = "Development";
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
